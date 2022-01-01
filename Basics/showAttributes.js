d63ba9b50e92ed150250d14cc8139e156a4f96f1
knambio <96670384+knambio@users.noreply.github.com>
2022-01-01 11:59:14 -0300

    Delete showAttributes.js

--- Basics/showAttributes.js
+++
@@ -1,3 +0,0 @@
-const HTMLElement = document.queryselector('img') //using img as an element example
-console.dir(HTMLElement)
-// this will produce an expandable object of the HTML element showing all its Attributes
